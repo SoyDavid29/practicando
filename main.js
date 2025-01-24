bd4b9908ff4e47b34e09feb3825c6b818342873a
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-01-24 14:25:27 -0600

    se remueve evento de ventana

--- main.js
+++ main.js
@@ -5,10 +5,6 @@ button.addEventListener('click', () =>{
 menu.classList.toggle('show');
 });
 
-window.addEventListener('beforeunload', (event) => {
-    event.preventDefault();
-    return '¿Estás seguro de que deseas salir?';
-});
 
 window.addEventListener('scroll', () => {
     console.log('El usuario está desplazándose por la página.');
